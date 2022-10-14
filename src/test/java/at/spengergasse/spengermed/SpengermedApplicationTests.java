455169e64aae7b9499e580ea591d5173f590ecd4
Maximilian Kraft <kraft@spengergasse.at>
2022-10-14 06:54:04 +0200

    added test

--- src/test/java/at/spengergasse/spengermed/SpengermedApplicationTests.java
+++ src/test/java/at/spengergasse/spengermed/SpengermedApplicationTests.java
@@ -1,5 +1,7 @@
 package at.spengergasse.spengermed;
 
+import at.spengergasse.spengermed.entities.Patient;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,4 +16,10 @@ class SpengermedApplicationTests {
 	MockMvc mockMvc;
 
 	// todo put your own tests here
+
+	@Test
+	public void testPatientInstantiation(){
+		assert Patient.generateExample() != null;
+	}
+
 }
